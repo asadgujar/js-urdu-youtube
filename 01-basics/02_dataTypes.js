abfce5ea35933b424d33b462406991b6c9b5a222
asadgujar <35732077+asadgujar@users.noreply.github.com>
2023-08-27 17:24:44 +0000

    check multiple data type and investigate types

---
+++ 01-basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // alert is no define inside node because i'm using node not browser thats why facing this issue 
+
+ //console.log (3 
+   // +
+   // 3
+   //  ); // please don't use this type practise becase im not read the code best thing is code readabilty should be high
+
+   let name = 'Asad ilyas'
+   let age = 25
+   let isLoggedIn = true
+   let state;
+
+
+  //  number => 2 to power 53
+  // bigint =>
+  // strng => ''
+  // boolean =>
+  // null => standalone value output is object
+  // undefined => output undefined
+  // symbol => uniquely identify
+
+
+  // object =?
+
+  console.log(typeof 'Asad ilyas')
+  console.log(typeof age);
+
+  console.table([name, age, isLoggedIn, state])
