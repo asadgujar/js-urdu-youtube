380ead2b0bd78afc90ee8343ed86c34f12366f5f
asadgujar <35732077+asadgujar@users.noreply.github.com>
2023-08-27 11:32:44 +0000

    working let const var data types

---
+++ 01-basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 123212321;
+let accountEmail = 'Asad@google.com';
+var accountPassword = '21322';
+accountCity = 'lahore';
+
+// value again assign and investigation process what will happed
+
+// accountId = 3;  // Not allowed to chane const value
+
+/* 
+    prefer not use to var because of issue in block scope and functionla scope
+*/
+
+accountEmail = 'ai@ai.com';
+accountPassword = '2121212';
+accountCity = 'Faisalabad';
+
+console.table([accountEmail, accountPassword, accountCity]);
+
+console.log(accountId);
