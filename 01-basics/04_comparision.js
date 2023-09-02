3b91cfeb6a2fbec460ee541942a777d896cdfe4f
asadgujar <35732077+asadgujar@users.noreply.github.com>
2023-09-02 06:04:27 +0000

    comparison code

---
+++ 01-basics/04_comparision.js
@@ -0,0 +1,38 @@
+// console.log(2 > 1)
+// console.log(2 < 1)
+// console.log(2 <= 1)
+// console.log(2 >= 1)
+// console.log(2 == 2)
+// console.log(2 === 2)
+
+// return true or false 
+
+// complexity 
+
+// console.log("2" > 1)
+// console.log("2" >= 1)
+// console.log('02' > 1)
+
+
+// AVOID COMAPRISON
+
+// console.log(null > 0)
+// console.log(null < 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+// console.log(null <= 0)
+// console.log(null === 0)
+
+// SAME UNDEFINED CASE 
+
+// console.log(undefined > 0)
+// console.log(undefined < 0)
+// console.log(undefined >= 0)
+// console.log(undefined <= 0)
+// console.log(undefined == 0)
+// console.log(undefined === 0)
+
+
+// piority clean code 
+
+
